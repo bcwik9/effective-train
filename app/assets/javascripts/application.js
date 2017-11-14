9dc30bf64b70adea21a3611aba3d959493f8d7f5
bcwik <bcwik9@gmail.com>
2017-11-14 23:48:59 +0000

    add bootstrap and quagga for scanners

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+// Bootstrap dependencies
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+//= require quagga.min
