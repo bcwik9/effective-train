7f6974ff5462b7baaeecbab51d597cc98c4661ff
bcwik <bcwik9@gmail.com>
2017-11-15 06:32:57 +0000

    some routes and handling for when a qr code is scanned

--- app/assets/javascripts/barcode_capture.js
+++ app/assets/javascripts/barcode_capture.js
@@ -2,6 +2,9 @@ $(function() {
     let scanner = new Instascan.Scanner({ video: document.getElementById('barcode-capture') });
     scanner.addListener('scan', function (content) {
         $("#barcode-result").text(content)
+        form = $("#build-list-form form")
+        form.find("#vendor_part_id").val(content)
+        form.submit()
     });
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "clients#dashboard"
 
-  get "client", to: "clients#dashboard"
+  get "dashboard", to: "clients#dashboard"
+  post "build_list", to: "clients#build_list"
 end
