7b7af5e2bec24cb3db447cc9c97d35210f9839ed
bcwik <bcwik9@gmail.com>
2017-12-01 04:40:06 +0000

    change camera order

--- app/assets/javascripts/barcode_capture.js
+++ app/assets/javascripts/barcode_capture.js
@@ -8,7 +8,7 @@ $(function() {
     });
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-            scanner.start(cameras[0]);
+            scanner.start(cameras[cameras.length - 1]);
         } else {
             console.error('No cameras found.');
         }
