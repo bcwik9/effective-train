ad91e4805ff43b6fd0d58d3a8f8c68524deae6ed
Ben Cwik <bcwik9@gmail.com>
2019-03-05 19:26:17 -0500

    Create barcode_capture.js

---
+++ barcode_capture.js
@@ -0,0 +1,15 @@
+$(function() {
+    let scanner = new Instascan.Scanner({ video: document.getElementById('barcode-capture') });
+    scanner.addListener('scan', function (content) {
+        $("#barcode-result").text(content)
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[cameras.length - 1]);
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function (e) {
+        console.error(e);
+    });
+})
