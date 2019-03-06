a759b31c708e1292b80c857fb048a0add69a2124
Ben Cwik <bcwik9@gmail.com>
2019-03-05 19:40:11 -0500

    Update barcode_capture.js

--- barcode_capture.js
+++ barcode_capture.js
@@ -1,15 +1,14 @@
-$(function() {
-    let scanner = new Instascan.Scanner({ video: document.getElementById('barcode-capture') });
-    scanner.addListener('scan', function (content) {
-        $("#barcode-result").text(content)
-    });
-    Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-            scanner.start(cameras[cameras.length - 1]);
-        } else {
-            console.error('No cameras found.');
-        }
-    }).catch(function (e) {
-        console.error(e);
-    });
-})
+
+let scanner = new Instascan.Scanner({ video: document.getElementById('barcode-capture') });
+scanner.addListener('scan', function (content) {
+  $("#barcode-result").text(content)
+});
+Instascan.Camera.getCameras().then(function (cameras) {
+  if (cameras.length > 0) {
+    scanner.start(cameras[cameras.length - 1]);
+  } else {
+    console.error('No cameras found.');
+  }
+}).catch(function (e) {
+  console.error(e);
+});
